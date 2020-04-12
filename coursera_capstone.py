61882ca43d15fb5c0a8db1e08c97651d245d1626
escadesupremo <49607535+escadesupremo@users.noreply.github.com>
2020-04-12 12:27:38 +0200

    first commit

---
+++ coursera_capstone.py
@@ -0,0 +1,5 @@
+git@github.com:escadesupremo/Coursera_Capstone.git
+#This is some markup text for Coursera Capstone Project!
+import pandas as pd
+import numpy as np
+print ("This is our Coursera Capstone Project!")
